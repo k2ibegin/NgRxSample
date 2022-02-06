bfa5b57426eb3f1023cc7a6ff244fdae807a687d
kuldeep adhikari <ad.kuldeep@gmail.com>
2022-02-06 14:12:19 +0100

    ngrx sample with angular13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './home/menu/menu.component';
+import { ShellComponent } from './home/shell/shell.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { PageNotFoundComponent } from './home/page-not-found/page-not-found.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+import { UserModule } from './user/user.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ShellComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    AppRoutingModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
